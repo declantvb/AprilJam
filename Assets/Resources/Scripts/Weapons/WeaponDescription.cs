23d9935d5898e10758a2670599363c17fed2949f
Declan <declantvb@gmail.com>
2016-04-16 15:25:01 +1200

    more guns

---
+++ Assets/Resources/Scripts/Weapons/WeaponDescription.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+[Serializable]
+public class WeaponDescription
+{
+	public GameObject BulletPrefab;
+	public WeaponType Type;
+	public float Damage;
+	public int ShellCount;
+	public float ShellSpeed;
+	public float Cooldown;
+	public float Accuracy;
+}
+
+public enum WeaponType
+{
+	Auto,
+	Shotgun
+}
