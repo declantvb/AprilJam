76237c72d1f4a88151c3729cdd736d10ee4d9eae
joelectronics <righteouslyalgebraic@gmail.com>
2016-04-17 11:06:07 +1200

    Added sprite sort orderer

--- Assets/Resources/Scripts/YPosSortOrderer.cs
+++ Assets/Resources/Scripts/YPosSortOrderer.cs
@@ -1,9 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[ExecuteInEditMode]
 public class YPosSortOrderer : MonoBehaviour
 {
     SpriteRenderer spriteRenderer;
+    [SerializeField] int Offset;
     [SerializeField] int CurrentSortOrder;
         
     void Start()
@@ -13,7 +15,7 @@ public class YPosSortOrderer : MonoBehaviour
     
     void Update()
     {
-        CurrentSortOrder = (int)(transform.position.y * 100);
-        spriteRenderer.sortingOrder = CurrentSortOrder;
+        CurrentSortOrder = (int)(-transform.position.y * 100);
+        spriteRenderer.sortingOrder = CurrentSortOrder + Offset;
     }
 }
