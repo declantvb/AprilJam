41127b0e8791eefbde608abbc293c72c5fd9dd21
Logan Krippner <logon.k13@gmail.com>
2016-04-16 16:29:03 +1200

    Started working on gun rotation

---
+++ Assets/Resources/Scripts/GunMover.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunMover : MonoBehaviour 
+{
+	[Header("Sprite offsets")]
+	[SerializeField] Vector2 offsetForGunForDownSprite;
+	[SerializeField] Vector2 offsetForGunForUpSprite;
+	[SerializeField] Vector2 offsetForGunForLeftSprite;
+	[SerializeField] Vector2 offsetForGunForRightSprite;
+
+	[Header("Sprites")]
+	[SerializeField] Sprite upGun;
+	[SerializeField] Sprite rightGun;
+	[SerializeField] Sprite downGun;
+	[SerializeField] Sprite leftGun;
+
+	[SerializeField] Transform gunTransform;
+	[SerializeField] SpriteRenderer gunSpriterenderer;
+
+	Sprite currentGunSprite;
+	Vector2 mousePosition;
+
+	void Start()
+	{
+		currentGunSprite = rightGun;
+	}
+
+	void Update()
+	{
+		mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		Vector2 direction = mousePosition - (Vector2)transform.position;
+		direction = direction.normalized;
+
+		UpdateGunRotation(direction);
+		UpdateSprite(direction);
+	}
+
+	void UpdateGunRotation(Vector2 direction)
+	{
+		float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+		gunTransform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+	}
+
+	void UpdateSprite(Vector2 direction)
+	{
+
+		if (direction == Vector2.zero)
+		{
+			//Special case
+		}
+		else//We are moving
+		{
+			if (Mathf.Abs(direction.y) >= Mathf.Abs(direction.x))//We are moving up or down
+			{
+				if (direction.y >= 0)//We are moving up
+				{
+					currentGunSprite = upGun;
+				}
+				else//We are moving down
+				{
+					currentGunSprite = downGun;
+				}
+			}
+			else //We are moving left or right
+			{
+				if (direction.x >= 0)//We are moving to the right
+				{
+					currentGunSprite = rightGun;
+				}
+				else//We are moving to the left
+				{
+					currentGunSprite = leftGun;
+				}
+			}
+
+			gunSpriterenderer.sprite = currentGunSprite;
+		}
+	}
+}
--- Assets/Resources/Scripts/PlayerController.cs
+++ Assets/Resources/Scripts/PlayerController.cs
@@ -27,6 +27,7 @@ public class PlayerController : MonoBehaviour
 	public int currentWeaponIndex = 0;
 	[SerializeField]
 	public WeaponDescription currentWeapon { get { return currentWeaponIndex < Weapons.Length ? Weapons[currentWeaponIndex] : null; } }
+	Quaternion rotation;
 
 
 	// Use this for initialization
@@ -87,10 +88,10 @@ public class PlayerController : MonoBehaviour
 
 		if (shootDirection.sqrMagnitude > 0.0f)
 		{
-			var rot = Quaternion.LookRotation(shootDirection, Vector3.forward);
-			rot.x = 0;
-			rot.y = 0;
-			transform.rotation = rot;
+			rotation = Quaternion.LookRotation(shootDirection, Vector3.forward);
+			rotation.x = 0;
+			rotation.y = 0;
+			//transform.rotation = rot;
 
 			if (shootCooldown < 0f && (joystickShooting || Input.GetAxis(shootAxis) > 0))
 			{
@@ -110,10 +111,10 @@ public class PlayerController : MonoBehaviour
 		switch (currentWeapon.Type)
 		{
 			case WeaponType.Auto:
-				MakeBullets(currentWeapon, bulletStart.transform.position, transform.rotation);
+				MakeBullets(currentWeapon, bulletStart.transform.position, rotation);
 				break;
 			case WeaponType.Shotgun:
-				MakeBullets(currentWeapon, bulletStart.transform.position, transform.rotation);
+				MakeBullets(currentWeapon, bulletStart.transform.position, rotation);
 				break;
 			default:
 				Debug.LogError("bad weapon type");
