9d33346757f059816762b54a660b210e7dc4decf
joelectronics <righteouslyalgebraic@gmail.com>
2016-04-17 10:59:45 +1200

    Rescaled sprites and added alien egg anims

---
+++ Assets/Resources/Scripts/SpriteAnimation.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteAnimation : MonoBehaviour
+{
+    [Header("Properties")]
+    [SerializeField] SpriteRenderer spriteRenderer;
+    [SerializeField] Sprite[] AnimationCells;
+    [SerializeField] float FrameDelay = 0.2f;
+    [SerializeField] int currentFrame;
+    [SerializeField] bool Playing;
+    [SerializeField] bool OneShotting;
+
+    float animElapsed;
+  
+    public bool IsPlaying
+    {
+        get
+        {
+            return Playing;
+        }
+    }
+
+    void Start()
+    {
+    }
+    
+    void Update()
+    {
+        if (Playing)
+        {
+            animElapsed += Time.deltaTime;
+            if (animElapsed >= AnimationCells.Length * FrameDelay)
+            {
+                animElapsed = 0;
+
+                //Stop the animation at end if this is a one-shot play
+                if (OneShotting)
+                {
+                    Playing = false;
+                    OneShotting = false;
+                }
+            }
+            else
+            {
+                //Update current animation frame
+                currentFrame = (int)(animElapsed / FrameDelay);
+                spriteRenderer.sprite = AnimationCells[currentFrame];
+            }
+        }
+    }
+
+    public void Play(bool isPlaying)
+    {
+        Playing = isPlaying;
+    }
+
+    public void PlayOneShot()
+    {
+        Playing = true;
+        OneShotting = true;
+        animElapsed = 0;
+    }
+}
---
+++ Assets/Resources/Scripts/YPosSortOrderer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class YPosSortOrderer : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+    [SerializeField] int CurrentSortOrder;
+        
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    
+    void Update()
+    {
+        CurrentSortOrder = (int)(transform.position.y * 100);
+        spriteRenderer.sortingOrder = CurrentSortOrder;
+    }
+}
