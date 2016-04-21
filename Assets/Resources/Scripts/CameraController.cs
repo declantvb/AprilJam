e15bc98f61709eadf73b85ef605fa7188a507ea4
Daniel Tebbutt <daniel.tebbutt@enlighten.co.nz>
2016-04-21 13:27:37 +1200

    Starting to work on this, maths is hard

--- Assets/Resources/Scripts/CameraController.cs
+++ Assets/Resources/Scripts/CameraController.cs
@@ -5,30 +5,30 @@ public class CameraController : MonoBehaviour
 {
 	private Transform Player;
 	private Transform PlayerAvatar;
-    private Controller PlayerController;
-    private bool zoomingOut;
-    private float zoomOut;
-    private float defaultSize;
+	private Controller PlayerController;
+	private bool zoomingOut;
+	private float zoomOut;
+	private float defaultSize;
 
-    private Camera localCamera;
+	private Camera localCamera;
 
-    public float MaxZoomout;
-    public float ZoomSpeed;
+	public float MaxZoomout;
+	public float ZoomSpeed;
 
-    // Use this for initialization
-    void Start()
-    {
-        localCamera = GetComponent<Camera>();
-        Player = transform.parent;
+	// Use this for initialization
+	void Start()
+	{
+		localCamera = GetComponent<Camera>();
+		Player = transform.parent;
 		PlayerAvatar = Player.GetComponentInChildren<PlayerController>().transform;
-        PlayerController = Player.GetComponentInChildren<PlayerController>().Controller;
-        defaultSize = localCamera.orthographicSize;
-    }
+		PlayerController = Player.GetComponentInChildren<PlayerController>().Controller;
+		defaultSize = localCamera.orthographicSize;
+	}
 
 	public void updateCameraViewport(Rect ViewportRect)
-    {
-        localCamera = GetComponent<Camera>();
-        localCamera.rect = ViewportRect;
+	{
+		localCamera = GetComponent<Camera>();
+		localCamera.rect = ViewportRect;
 	}
 
 	// Update is called once per frame
@@ -36,15 +36,24 @@ public class CameraController : MonoBehaviour
 	{
 		transform.position = PlayerAvatar.transform.position + Vector3.back;
 
-        // Show map by zooming out
-        zoomingOut = Input.GetAxis(PlayerController.ShowMapAxis) > 0;
+		// Show map by zooming out
+		zoomingOut = Input.GetAxis(PlayerController.ShowMapAxis) > 0;
 
-        if (zoomingOut && zoomOut <= MaxZoomout) zoomOut += Time.deltaTime * ZoomSpeed;
-        if (!zoomingOut && zoomOut >= 0) zoomOut -= Time.deltaTime * ZoomSpeed;
-        if (zoomOut > MaxZoomout) zoomOut = MaxZoomout;
-        if (zoomOut < 0) zoomOut = 0;
+		if (zoomingOut && zoomOut <= MaxZoomout) zoomOut += Time.deltaTime * ZoomSpeed;
+		if (!zoomingOut && zoomOut >= 0) zoomOut -= Time.deltaTime * ZoomSpeed;
+		if (zoomOut > MaxZoomout) zoomOut = MaxZoomout;
+		if (zoomOut < 0) zoomOut = 0;
 
-        localCamera.orthographicSize = defaultSize * (1 + zoomOut);
-        
-    }
+		localCamera.orthographicSize = defaultSize * (1 + zoomOut);
+
+		var size = localCamera.ViewportToScreenPoint(Vector3.up) - localCamera.ViewportToScreenPoint(Vector3.zero);
+		
+
+		var snapSize = size.y / ((int)(size.y / 16) * 16) * localCamera.orthographicSize;
+		var snapPosition = new Vector3((int)(transform.position.x * 32), (int)(transform.position.y * 32), transform.position.z * 32) / 32f;
+
+		Debug.Log(size + "   - " + localCamera.orthographicSize + " - " + snapSize + " - " + transform.position + " - " + snapPosition);
+		localCamera.orthographicSize = snapSize;
+		transform.position = snapPosition;
+	}
 }
